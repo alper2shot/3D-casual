371342fef549e34dfbf04ab358e3598bfd33a6a2
alper2shot <67012524+alper2shot@users.noreply.github.com>
2021-07-14 16:56:19 +0300

    texture, torque and bricks

--- 3D-casual/Assets/Scripts/GameController.cs
+++ 3D-casual/Assets/Scripts/GameController.cs
@@ -8,6 +8,7 @@ public class GameController : MonoBehaviour
     private Vector3 ballStartPos;
     public GameObject ball;
     Rigidbody ballrb;
+    public int ballCount;
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +20,21 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (canSpawnBall)
+        if(ballCount == 0)
+        {
+            Destroy(ball, 5f);
+        }
+
+        if (canSpawnBall && ballCount >0)
         {
             ball.transform.position = ballStartPos;
             canSpawnBall = false;
+            ballCount--;
             ballrb.isKinematic = true;
+            ball.GetComponent<Swipe>().worked = false;
+            ball.GetComponent<Swipe>().isTouched = false;
         }
+
+        
     }
 }
--- 3D-casual/Assets/Scripts/Swipe.cs
+++ 3D-casual/Assets/Scripts/Swipe.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class Swipe : MonoBehaviour
 {
-
-
+    public GameObject controller;
+    public bool worked=false;
+    public bool isTouched = false;
     Vector2 startPos, endPos, direction;
     float touchTimeStart, touchTimeFinish, timeInterval;
+
     [SerializeField]
     float throwForceInXandY = 1f;
+
+  
 
     [SerializeField]
     float throwForceInZ = 250f;
@@ -20,42 +24,68 @@ public class Swipe : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        
+    }
+
+    IEnumerator Ball()
+    {
+        yield return new WaitForSeconds(5);
+        controller.GetComponent<GameController>().canSpawnBall = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
+        if(Input.touchCount > 0 && !isTouched && Input.GetTouch(0).phase == TouchPhase.Began)
         {
             touchTimeStart = Time.time;
             startPos = Input.GetTouch(0).position;
         }
 
-        if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended))
+        if (Input.touchCount > 0 && !isTouched && Input.GetTouch(0).phase == TouchPhase.Ended)
         {
             touchTimeFinish = Time.time;
             timeInterval = touchTimeFinish - touchTimeStart;
             endPos = Input.GetTouch(0).position;
             direction = startPos - endPos;
             rb.isKinematic = false;
-            rb.AddForce(-direction.x * throwForceInXandY, -direction.y * throwForceInXandY, throwForceInZ / timeInterval);
-
+            if (timeInterval > 0.2f)
+            {
+                rb.AddForce(-direction.x * throwForceInXandY, -direction.y * throwForceInXandY, throwForceInZ / timeInterval);
+                
+            }
+            isTouched = true;
         }
 
-        if ( Input.GetMouseButtonDown(0)) {
+        if ( Input.GetMouseButtonDown(0)&& !isTouched) {
             touchTimeStart = Time.time;
             startPos = Input.mousePosition;
         }
 
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0) && !isTouched)
         {
             touchTimeFinish = Time.time;
             timeInterval = touchTimeFinish - touchTimeStart;
             endPos = Input.mousePosition;
             direction = startPos - endPos;
             rb.isKinematic = false;
-            rb.AddForce(-direction.x * throwForceInXandY, -direction.y * throwForceInXandY, throwForceInZ / timeInterval);
-            
+            if (timeInterval > 0.2f)
+            {
+                rb.AddForce(-direction.x * throwForceInXandY, -direction.y * throwForceInXandY, throwForceInZ / timeInterval);
+                rb.AddTorque(Vector3.right * throwForceInZ / timeInterval, ForceMode.Force);
+            }
+            isTouched = true;
+        }
+
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(!worked)
+        {
+            StartCoroutine(Ball());
+            worked = true;
         }
     }
 }
