209266a76d2b3380428d758d0bd32b32bbc70af6
alper2shot <67012524+alper2shot@users.noreply.github.com>
2021-07-19 22:59:59 +0300

    Scene managment-1

--- 3D-casual/Assets/Scripts/Plane.cs
+++ 3D-casual/Assets/Scripts/Plane.cs
@@ -7,13 +7,7 @@ public class Plane : MonoBehaviour
     public GameObject controller;
     
   
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-
-    }
-
+  
 
     private void OnCollisionEnter(Collision collision)
     {
---
+++ 3D-casual/Assets/Scripts/SceneManage.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManage : MonoBehaviour
+{
+    public bool canLoad;
+    // Update is called once per frame
+    void Update()
+    {
+        if (canLoad)
+        {
+            SceneManager.Load
+        }
+    }
+}
