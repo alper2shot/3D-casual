d91910a4f90d17a6b2c76162e368db2ffe25e71a
alper2shot <67012524+alper2shot@users.noreply.github.com>
2021-08-10 21:02:11 +0300

    interstitial ad 1

--- 3D-casual/Assets/Scripts/AdMobScript.cs
+++ 3D-casual/Assets/Scripts/AdMobScript.cs
@@ -8,20 +8,22 @@ public class AdMobScript : MonoBehaviour
 {
     //string appID = "ca-app-pub-9789420081213637~9306642947";
 
-    string BannerAdID = "ca-app-pub-3940256099942544/6300978111";
-    //string IntersitialAdID = "ca-app-pub-3940256099942544/1033173712";
+    //string BannerAdID = "ca-app-pub-3940256099942544/6300978111";
+    string IntersitialAdID = "ca-app-pub-3940256099942544/1033173712";
     //string RewarededAdID = "ca-app-pub-3940256099942544/5224354917";
 
-    private BannerView bannerView;
-    //private InterstitialAd intersitial;
+    //private BannerView bannerView;
+    private InterstitialAd interstitial;
     //private RewardedAd rewarded;
 
     // Start is called before the first frame update
     void Start()
     {
-        MobileAds.Initialize(initStatus => { }); 
+        MobileAds.Initialize(initStatus => { });
+        RequestInterstitial();
     }
 
+    /*
     public void RequestBanner()
     {
         /*
@@ -32,7 +34,7 @@ public class AdMobScript : MonoBehaviour
 #else
             string adUnitId = "unexpected_platform";
 #endif
-        */
+       
 
         // Create a 320x50 banner at the top of the screen.
         this.bannerView = new BannerView(BannerAdID, AdSize.Banner, AdPosition.Top);
@@ -45,16 +47,17 @@ public class AdMobScript : MonoBehaviour
         this.bannerView.OnAdOpening += this.HandleOnAdOpened;
         // Called when the user returned from the app after an ad click.
         this.bannerView.OnAdClosed += this.HandleOnAdClosed;
+
         /*
-         google ads 6.0 error
+         //google ads 6.0 error
         // Called when the ad click caused the user to leave the application.
-        this.bannerView.OnAdLeavingApplication += this.HandleOnAdLeavingApplication;
-        */
+        //this.bannerView.OnAdLeavingApplication += this.HandleOnAdLeavingApplication;
+        
 
         ShowBannerAD();
         
     }
-
+    
     public void ShowBannerAD()
     {
         // Create an empty ad request.
@@ -62,6 +65,46 @@ public class AdMobScript : MonoBehaviour
 
         // Load the banner with the request.
         this.bannerView.LoadAd(request);
+    }
+    */
+
+    public void RequestInterstitial()
+    {
+        /*
+#if UNITY_ANDROID
+        string adUnitId = "ca-app-pub-3940256099942544/1033173712";
+#elif UNITY_IPHONE
+        string adUnitId = "ca-app-pub-3940256099942544/4411468910";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+        */
+        // Initialize an InterstitialAd.
+        this.interstitial = new InterstitialAd(IntersitialAdID);
+        // Called when an ad request has successfully loaded.
+        this.interstitial.OnAdLoaded += HandleOnAdLoaded;
+        // Called when an ad request failed to load.
+        this.interstitial.OnAdFailedToLoad += HandleOnAdFailedToLoad;
+        // Called when an ad is shown.
+        this.interstitial.OnAdOpening += HandleOnAdOpened;
+        // Called when the ad is closed.
+        this.interstitial.OnAdClosed += HandleOnAdClosed;
+
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the interstitial with the request.
+        this.interstitial.LoadAd(request);
+
+       
+    }
+
+    public void LoadInterstitial()
+    {
+        if (this.interstitial.IsLoaded())
+        {
+            this.interstitial.Show();
+        }
     }
 
     public void HandleOnAdLoaded(object sender, EventArgs args)
